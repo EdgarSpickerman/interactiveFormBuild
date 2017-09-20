2629a12215e4cc8a8ef43b2fe2155812bb053122
edgar <spickerman.edgar@gmail.com>
2017-09-19 21:32:44 -0700

    added the deafault behavior also added the design change event

--- js/app-main.js
+++ js/app-main.js
@@ -1,1 +1,30 @@
-// JavaScript source code
+/* ********************************************************functions******************************************************************* */
+function showColors(design) {
+    $('select#color').html('');
+    for (var i = 0; i < $colorOptions.length; i++) {
+        if ($colorOptions[i].innerHTML.includes(design)) {
+            $('select#color').append($colorOptions[i].outerHTML);
+        }
+    }
+}
+
+/* ********************************************************Validation functions******************************************************** */
+
+/* ********************************************************Events******************************************************** */
+$('#other-title').change(() => {
+
+});
+
+$('#design').change(() => {
+    let selectedDesign = $('select#design option:selected').html();
+    selectedDesign !== 'Select Theme' ? $('#colors-js-puns').show() : $('#colors-js-puns').hide();
+    selectedDesign === 'Theme - JS Puns' ? showColors('JS Puns shirt only') : showColors('JS shirt only');
+}); //1) gets the html of the selected option. 2) color options show if any theme is selected 3) displays only the colors for each design.
+
+/* ********************************************************Global scope and default actions******************************************************* */
+$('input#name').focus;                                                    //focus on the first text area
+$('#other-title').attr('style', 'display:none');                          //hiding the other job role text field
+$('#colors-js-puns').attr('style', 'display:none');                        // hiding the color options 
+$('#payment').val('credit card');                                         // selecting the cc as default
+$('div p').attr('style', 'display:none');                                 // hiding the paypal and bitcoin text since cc is default option
+$colorOptions = $('#color option');                                       // get all availble color choices
